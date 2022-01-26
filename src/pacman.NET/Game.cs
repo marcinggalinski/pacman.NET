d00d0f8615232cf831cac6d139ee2681a6e7b4b2
Marcin Galiński <marcinggalinski@gmail.com>
2022-01-26 21:27:17 +0100

    displaying window

---
+++ src/pacman.NET/Game.cs
@@ -0,0 +1,27 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+
+namespace pacman.NET;
+
+public static class Game
+{
+    private static readonly RenderWindow Window = new RenderWindow(new VideoMode(800, 600), "Pacman.NET");
+
+    public static void Main()
+    {
+        Window.Closed += (sender, args) =>
+        {
+            var window = sender as RenderWindow;
+            window!.Close();
+        };
+        
+        while (Window.IsOpen)
+        {
+            Window.Clear();
+            
+            Window.DispatchEvents();
+            
+            Window.Display();
+        }
+    }
+}
